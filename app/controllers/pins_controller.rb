c2717d427f655c7e0662f5926a2c1a173320d586
Amir Kanpurwala <akanpurwala@gmail.com>
2014-06-11 14:22:35 -0700

    Updated limit for number of Pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,7 @@ class PinsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @pins = Pin.all.order("created_at DESC").limit(3) 
+    @pins = Pin.all.order("created_at DESC") 
   end
 
   def show
